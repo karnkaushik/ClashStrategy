ac93a09516d00d152c6fe6d8ad37df9cda714ab2
karnkaushik <karankaushik123kk@gmail.com>
2026-08-05 23:59:53 +0530

    Delete app directory

--- app/src/main/java/com/clashgame/strategy/model/DefenseTower.kt
+++
@@ -1,5 +0,0 @@
-package com.clashgame.strategy.model
-
-class DefenseTower : Building("Archer Tower") {
-    var defensePower: Int = 35
-}
--- app/src/main/java/com/clashgame/strategy/model/Guild.kt
+++
@@ -1,9 +0,0 @@
-package com.clashgame.strategy.model
-
-class Guild(val guildName: String) {
-    val members = mutableListOf<Player>()
-
-    fun addMember(player: Player) {
-        members.add(player)
-    }
-}
